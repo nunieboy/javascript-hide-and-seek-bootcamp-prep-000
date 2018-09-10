508968a316a6da6154a81f1ed079f16ae2b00232
Pablo Eusebio <nunieboy88@gmail.com>
2018-09-10 18:02:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,6 +35,6 @@ function deepestChild(){
 function increaseRankBy(n){
   const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
   for (let i = 0; i < lis.length; i++) {
-    parseInt(lis[i].innerHTML) = (i + parseInt(n))
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n 
   }
 }
