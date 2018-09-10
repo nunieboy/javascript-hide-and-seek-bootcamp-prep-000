54d76a37e5a01b891f144e485ac7be9ad0d92f96
Pablo Eusebio <nunieboy88@gmail.com>
2018-09-10 17:56:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector('div.target')
+}
+
+
+
+function deepestChild(){
+  let divList = document.getElementById('grand-node');
+  let nextNode = divList.children[0]
+
+  while (nextNode){
+  divList = nextNode
+  nextNode = divList.children[0]
+  }
+
+  return divList
+}
+
+
+//function deepestChild(){
+//  let divList = document.getElementById('grand-node')
+  //while (divList.chilren[0]){
+  //  div
+  //let nextNode = divList[i]
+  //}
+  //
+  //return nextNode
+//}
+
+function increaseRankBy(n){
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (i + parseInt(n)).toString()
+  }
+}
